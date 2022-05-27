50456c7ad3f07c3f7253e55f60dba8dab86f2de0
elianvilar <92454386+elianvilar@users.noreply.github.com>
2022-05-27 14:19:10 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
 alert('Hola bienvenido a la web de T4N paaaa');
 alert('Todo bien? Me alegro');
 alert('Web en beta');
-window.open('https://cdn.discordapp.com/attachments/976970536884273222/977342850649956382/T4N_relases.rar');
